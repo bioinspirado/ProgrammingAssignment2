431e323c7a3c754c30d6c7866eb507454e3f274f
Diego Moreira <diego@bioinspirado.com>
2017-07-24 09:38:54 -0300

    Finally complete!

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
+#Function who recover a cached matrix, contain a get and set statement. 
+#Care with chenges in original matrix, can cause inconsistency in cache.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverso <- NULL
+        
+        set <- function(mt){
+                x <<- mt
+                inverso <<- NULL
+        }
+        get <- function() x
+        
+        setInverso <- function(setinverso) inverso <- setinverso
+        getInverso <- function() inverso
+        
+        list(set = set, 
+             get = get, 
+             setInverso = setInverso,
+             getInverso = getInverso)
 }
-
 
 ## Write a short comment describing this function
 
+#verify if have a cache from matriz, and return if find.
+#Else, solve the inversion, store i cache and return the correctly answer.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        reverso <- x$getInverso()
+        if(!is.null(reverso)){
+                message('Data already in cache!')
+                message('Recovering from cache...')
+                return(reverso)
+        }else{
+                dados <- x$get()
+                reverso <- solve(data, ...)
+                x$setInverso(reverso)
+                return(reverso)        
+        }
 }
